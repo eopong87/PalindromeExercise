ba8ba7fe2e4be2c5e10340f61e599204d0714ccf
eopong87 <eopong87@gmail.com>
2023-07-14 15:55:23 -0400

    palindrome

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsPalindrome(string orginalWord)
+        {
+            string reversedWord = String.Empty;
+            for (int i = orginalWord.Length - 1; i >= 0; i++)
+            {
+                reversedWord += orginalWord[i];
+
+            }
+            return orginalWord == reversedWord;
+        }
+
+    }
+}
+
+
+
+
+
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hannah",true)]
+        [InlineData("hector", false)]
+        
+        public void IsPalindromeTest( string someword, bool expected)
         {
-
+            //arrange
+            var instance = new WordSmith();
+            //act
+           var actual = instance.IsPalindrome(someword);
+            //assert
+            Assert.Equal(expected, actual);
         }
-    }
+    }  
 }
